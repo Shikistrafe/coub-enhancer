480bc549ee43d88e93f1b592fb6a4397e5b381a9
Shikistrafe <asyru13@gmail.com>
2022-12-20 10:40:41 +0300

    small changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ console.log("app.js loaded")
 
 function App() {
 	return (
-    	<div className="content">
+		<div className="content">
 			<div className="header">
 				<p>Coub Enhaner</p>
 			</div>
 		</div>
 	);
 }
-
 export default App;
