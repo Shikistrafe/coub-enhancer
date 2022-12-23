87382042093d49e93cb6b6a7ee3fffc2f957f277
Shikistrafe <asyru13@gmail.com>
2022-12-23 16:05:34 +0300

    add base visual settings

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-console.log("app.js loaded")
+// dev information
+console.log(navigator.userAgent);
 
 function App() {
 	return (
 		<div className="content">
-			<h1 class="header"><a href="https://coub.com/" class="link-to-coub">Coub Enhancer</a></h1>
+			<h1 className="header"><a href="https://coub.com/" className="link-to-coub">Coub Enhancer</a></h1>
+			<div className="visual">
+				<ul>
+					<li>Coub Layout: <select name="" id="">
+							<option value="">Default</option>
+							<option value="">YouTube</option>
+							<option value="">Twitch</option>
+						</select>
+					</li>
+					<li>Coub style: <select name="" id="">
+							<option value="">Default</option>
+							<option value="">Compact</option>
+							<option value="">"Zen"</option>
+						</select>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
